a821ac1c80678b242e017bc183a8e83e7c29024b
Nikita <nikitasysoev51@gmail.com>
2021-05-31 16:26:40 +0300

    Добавляет утилитарные функции

---
+++ js/main.js
@@ -0,0 +1,11 @@
+const getRandomInteger = (min = 0, max = 1) => {
+  const lower = Math.ceil(Math.min(min, max));
+  const upper = Math.floor(Math.max(min, max));
+
+  return Math.floor(lower + Math.random() * (upper - lower + 1));
+};
+const getIsStringLonger = (str, maxLength) => str.length <= maxLength ;
+
+getIsStringLonger('ZPfpfk', 140);
+
+getRandomInteger(1, 10);
